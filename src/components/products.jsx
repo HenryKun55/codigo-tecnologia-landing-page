69985306d9ec59e283f94b0b4af75accd51b289d
Flávio Henrique <henriqueflavio55@gmail.com>
2021-09-27 21:43:06 -0300

    Separate products by type

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -5,7 +5,9 @@ export const Products = (props) => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
 
-  const images = props.data.map((obj) => obj.thumb.replace("-small", "-large"));
+  const images = props.data.map(({ products }) =>
+    products.map(({ thumb }) => thumb.replace("-small", "-large"))
+  );
 
   const openImageViewer = useCallback((index) => {
     setCurrentImage(index);
@@ -25,56 +27,69 @@ export const Products = (props) => {
         </div>
         <div className="row">
           <div className="products-items">
-            {props.data.map(
-              (
-                {
-                  title,
-                  thumb,
-                  descriptions,
-                  selections,
-                  warranty,
-                  use,
-                  detail,
-                },
-                index
-              ) => (
-                <div
-                  key={index}
-                  onClick={() => openImageViewer(index)}
-                  className="col-sm-6 col-md-4 col-lg-4 product"
-                >
-                  <div className="products-item cursor">
-                    <div className="hover-bg">
-                      <div className="hover-text">
-                        <div className="text">
-                          <h4>{title}</h4>
-                          {descriptions && (
-                            <ul>
-                              {descriptions &&
-                                descriptions.map((description, index) => (
-                                  <li key={index}>{description}</li>
-                                ))}
-                            </ul>
-                          )}
-                          {selections && (
-                            <ul>
-                              {selections &&
-                                selections.map((selection, index) => (
-                                  <li key={index}>{selection}</li>
-                                ))}
-                            </ul>
-                          )}
-                          <p>{detail}</p>
-                          <h4>{warranty}</h4>
-                          <h4>{use}</h4>
+            {props.data.map(({ type, products }) => (
+              <>
+                <div className="title-products">
+                  <h3>{type}</h3>
+                </div>
+                <div className="products">
+                  {products.map(
+                    (
+                      {
+                        title,
+                        descriptions,
+                        selections,
+                        detail,
+                        warranty,
+                        use,
+                        thumb,
+                      },
+                      index
+                    ) => (
+                      <div
+                        key={index}
+                        onClick={() => openImageViewer(index)}
+                        className="col-sm-6 col-md-4 col-lg-4 product"
+                      >
+                        <div className="products-item cursor">
+                          <div className="hover-bg">
+                            <div className="hover-text">
+                              <div className="text">
+                                <h4>{title}</h4>
+                                {descriptions && (
+                                  <ul>
+                                    {descriptions &&
+                                      descriptions.map((description, index) => (
+                                        <li key={index}>{description}</li>
+                                      ))}
+                                  </ul>
+                                )}
+                                {selections && (
+                                  <ul>
+                                    {selections &&
+                                      selections.map((selection, index) => (
+                                        <li key={index}>{selection}</li>
+                                      ))}
+                                  </ul>
+                                )}
+                                <p>{detail}</p>
+                                <h4>{warranty}</h4>
+                                <h4>{use}</h4>
+                              </div>
+                            </div>
+                            <img
+                              src={thumb}
+                              className="img-responsive"
+                              alt={title}
+                            />{" "}
+                          </div>
                         </div>
                       </div>
-                      <img src={thumb} className="img-responsive" alt={title} />{" "}
-                    </div>
-                  </div>
+                    )
+                  )}
                 </div>
-              )
-            )}
+              </>
+            ))}
           </div>
 
           {isViewerOpen && (
