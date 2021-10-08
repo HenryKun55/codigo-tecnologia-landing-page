191959135365102230d3b6f533a9e35dbd0c3ada
Flávio Henrique <henriqueflavio55@gmail.com>
2021-10-07 21:47:16 -0300

    Add redirects

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
           <Depoiments data={data.Depoiments} />
         </Route>
         <Route exact path="/contato">
-          {/* <Footer /> */}
+          <Footer />
         </Route>
       </Switch>
       <div className="square">
